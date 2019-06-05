e777264f791ce0cb5aa1a9edfd6af212a1240487
Gary <cam.dev2019@gmail.com>
2019-06-05 20:24:27 +0100

    code finished apart minor styling

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ keycode = () => {
     let key = event.keyCode;
     let keyStroke = event.key;
     let keyCode = event.code;
-    // console.log(key);
-    // console.log(keyStroke);
-    // console.log(keyCode);
-
     document.getElementById("key-result").innerHTML = keyStroke;
     document.getElementById("event-result").innerHTML = key;
     document.getElementById("event-code").innerHTML = keyCode;
